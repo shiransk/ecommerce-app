161d3b10e062c2f418b2711e285324ea62b1be95
Shiran Koriat <shiransk@gmail.com>
2016-07-26 21:31:43 -0700

    refactor with model methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,19 @@
 class Product < ApplicationRecord
+
+  def sale_message
+    if price.to_i < 2
+       "Discount Item!"
+    else
+      "On Sale!"
+    end
+  end
+
+  def tax
+    price.to_i * 0.09
+  end
+
+  def total
+    price.to_i + tax
+  end
+
 end
